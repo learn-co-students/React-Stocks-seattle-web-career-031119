8135146e2b87c0796115e5a377bad93730e813ea
quinncidences <qrc.inpa@gmail.com>
2019-05-20 17:53:39 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={(props.filter === "Alphabetically" ? true : false)} onChange={(ev) => props.grabSortFilter(ev)}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={(props.filter === "Price" ? true : false)} onChange={(ev) => props.grabSortFilter(ev)}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(ev) => props.grabIndustryFilter(ev)}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
